29907f6123f64a0279d6d73c723cc6e3a739b660
Fakile Razaq <razaq_jimmie@yahoo.com>
2025-07-12 13:29:12 +0100

    Connect To DB

--- app.js
+++ app.js
@@ -1,5 +1,24 @@
+//require('dotenv').config();
+//import 'dotenv/config' For ES6
+
+const dotenv = require('dotenv');
 const express = require('express');
+const mongoose = require("mongoose");
+
+dotenv.config();
+
+// Express App
 const app = express();
+
+// Connect to DB(mongodb) and listen for requests
+const dbURI = process.env.dbURI;
+mongoose.connect(dbURI)
+	.then((result) => app.listen(5000, () => {
+		console.log("Successfully Connected to Database!");
+	}))
+	.catch((error) => {
+		console.log(error);
+	})
 
 
 app.get('/', (req, res) =>{
