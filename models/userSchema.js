5f46ec9b1b40b12a362fc896525c20032956f60c
Fakile Razaq <razaq_jimmie@yahoo.com>
2025-07-14 12:55:13 +0100

    Built DB schema

---
+++ models/userSchema.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+
+const userSchema = new Schema({
+	name: {
+		type: String,
+		required: true
+	},
+	email: {
+		type: String,
+		required: true
+	},
+	password: {
+		type: String,
+		required: true
+	},
+	address: {
+		type: String,
+		required: true
+	},
+	number: {
+		type: Number,
+		required: true
+	},
+}, { timestamps: true });
+
+
+const User = Schema.model('User', userSchema);
+module.exports = User;
+
