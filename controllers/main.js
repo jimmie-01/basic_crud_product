1c819f16d394432de76fb77cb1feaf1f3631223e
Fakile Razaq <razaq_jimmie@yahoo.com>
2025-07-15 14:38:19 +0100

    The 'About' and 'Add User' page

--- controllers/main.js
+++ controllers/main.js
@@ -7,3 +7,17 @@ const express = require('express');
 module.exports.get_home = (req, res) => {
 	res.render('index');
 }
+
+/**
+ * GET - About Page
+ */
+module.exports.get_about = (req, res) => {
+	res.render('about');
+}
+
+/**
+ * GET - Add User Page
+ */
+module.exports.get_create_user = (req, res) => {
+	res.render('add_user');
+}
--- server/routes/main_route.js
+++ server/routes/main_route.js
@@ -3,5 +3,7 @@ const router = express.Router();
 const Controller = require('../../controllers/main');
 
 router.get('/', Controller.get_home);
+router.get('/about', Controller.get_about);
+router.get('/user/create', Controller.get_create_user);
 
 module.exports = router;
